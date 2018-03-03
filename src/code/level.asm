5ce6bb93232dfdbd780a4501cbfe94def4fd37cf
dannye <corrnondacqb@yahoo.com>
2018-03-03 07:33:14 -0600

    Add notes!

--- src/code/level.asm
+++ src/code/level.asm
@@ -1,14 +1,9 @@
 section "level wram", wram0
+wNotePtr: ds 2
+wNextNote: ds 1
+wDelay: ds 1
 
 section "levelscreen", rom0
-LEVEL_X = 2
-LEVEL_Y = 2
-LEVEL2_X = 15
-LEVEL2_Y = 15
-BUTTON_BAR_HEIGHT = 3
-BUTTON_BAR_WIDTH = 16
-BAR_SECTION_HEIGHT = 12
-BAR_SECTION_WIDTH = 7
 
 LevelScreen::
 	call DisableLCD
@@ -16,14 +11,160 @@ LevelScreen::
 	call EnableLCD
 	xor a
 	call PlaySound
+	ld hl, Level1Notes
+	ld a, [hli]
+	ld [wNextNote], a
+	ld a, [hli]
+	ld [wDelay], a
+	ld a, l
+	ld [wNotePtr], a
+	ld a, h
+	ld [wNotePtr + 1], a
 .loop
 	call WaitVBlank
-	jr .loop	
+	ld a, [wNextNote]
+	cp $ff
+	jr z, .advanceNotes ; song is over
+	ld a, [wDelay]
+	and a
+	jr z, .beginNote
+	dec a
+	ld [wDelay], a
+	jr .advanceNotes
+.beginNote
+	;;; spawn new note sprite
+	ld c, 40
+	ld hl, wOAM + 1
+.findSpaceLoop
+	ld a, [hl]
+	and a
+	jr z, .found
+	inc hl
+	inc hl
+	inc hl
+	inc hl
+	dec c
+	jr nz, .findSpaceLoop
+	;;; error, all notes full, ignore new note
+	jr .skip
+.found
+	dec hl
+	;;; create note at hl
+	ld a, [wNextNote]
+	cp D_LEFT
+	ld b, 16*1 + 8
+	jr z, .cont
+	cp D_UP
+	ld b, 16*2 + 8
+	jr z, .cont
+	cp D_RIGHT
+	ld b, 16*3 + 8
+	jr z, .cont
+	cp D_DOWN
+	ld b, 16*4 + 8
+	jr z, .cont
+	cp B_BUTTON
+	ld b, 16*5 + 8
+	jr z, .cont
+	;cp A_BUTTON
+	ld b, 16*6 + 8
+	;jr z, .cont
+.cont
+	ld a, 8
+	ld [hli], a
+	ld a, b
+	ld [hli], a
+	xor a
+	ld [hli], a
+	ld [hli], a
+.skip
+	ld a, [wNotePtr]
+	ld l, a
+	ld a, [wNotePtr + 1]
+	ld h, a
+	ld a, [hli]
+	ld [wNextNote], a
+	ld a, [hli]
+	ld [wDelay], a
+	ld a, l
+	ld [wNotePtr], a
+	ld a, h
+	ld [wNotePtr + 1], a
+.advanceNotes
+	;;; for all visible notes, scroll down
+	ld c, 40
+	ld hl, wOAM
+.advanceLoop
+	ld a, [hl]
+	and a
+	jr z, .skipAdvance
+	inc a
+	cp 160
+	jr nc, .dead
+	ld [hl], a
+	jr .skipAdvance
+.dead
+	xor a
+	ld [hli], a
+	ld [hli], a
+	ld [hli], a
+	ld [hli], a
+	jr .skipInc
+.skipAdvance
+	inc hl
+	inc hl
+	inc hl
+	inc hl
+.skipInc
+	dec c
+	jr nz, .advanceLoop
+	jp .loop
+
+Level1Notes:
+	db D_LEFT, 0
+	db D_UP, 20
+	db D_RIGHT, 20
+	db B_BUTTON, 20
+	db B_BUTTON, 20
+	db D_RIGHT, 20
+	db B_BUTTON, 20
+	db A_BUTTON, 20
+	db D_DOWN, 20
+	db A_BUTTON, 50
+	db A_BUTTON, 20
+	db D_RIGHT, 20
+	db B_BUTTON, 20
+	db A_BUTTON, 20
+	db D_DOWN, 20
+	db B_BUTTON, 20
+	db D_DOWN, 20
+	db A_BUTTON, 20
+	db D_RIGHT, 20
+	db B_BUTTON, 20
+	db A_BUTTON, 20
+	db D_UP, 20
+	db D_RIGHT, 20
+	db B_BUTTON, 20
+	db A_BUTTON, 20
+	db D_RIGHT, 20
+	db D_DOWN, 20
+	db A_BUTTON, 50
+	db A_BUTTON, 50
+	db D_UP, 20
+	db A_BUTTON, 20
+	db D_RIGHT, 20
+	db D_DOWN, 20
+	db B_BUTTON, 20
+	db $FF, $FF
 
 LoadLevelGraphics:
 	ld bc, LevelGraphicsEnd - LevelGraphics
 	ld hl, LevelGraphics
 	ld de, vChars1
+	call CopyData
+	ld bc, NoteGraphicsEnd - NoteGraphics
+	ld hl, NoteGraphics
+	ld de, vChars0
 	call CopyData
 	
 	call DrawLevelTiles
@@ -48,7 +189,6 @@ DrawLevelTiles:
 	dec c
 	jr nz, .loop
 	ret
-	
 
 LevelTileMap:
 	db $00, $90, $A1, $90, $A1, $90, $A1, $90, $A1, $90, $A1, $90, $A1, $90, $00, $00
@@ -73,3 +213,7 @@ LevelTileMapEnd:
 LevelGraphics:
 	INCBIN "gfx/level.2bpp"
 LevelGraphicsEnd:
+
+NoteGraphics:
+	INCBIN "gfx/note.2bpp"
+NoteGraphicsEnd:
