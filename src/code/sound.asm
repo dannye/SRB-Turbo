bc402a7ebaade37d0a708ecb713d06c378ebe8c8
dannye <corrnondacqb@yahoo.com>
2018-03-03 19:25:14 -0600

    Notes and rests finally work

--- src/code/sound.asm
+++ src/code/sound.asm
@@ -1,74 +1,79 @@
 section "sound wram", wram0
 wCh0Ptr: ds 2
-wCh0Pitch: ds 2
+wCh0Pitch: ds 1
 wCh0Length: ds 1
+wCh0Octave: ds 1
 
 section "sound", rom0
 C_: macro
-	dw 11
+	db 0
 	db \1
 endm
 
 C#: macro
-	dw $F89D
+	db 1
 	db \1
 endm
 
 D_: macro
-	dw $F907
+	db 2
 	db \1
 endm
 
 D#: macro
-	dw $F96B
+	db 3
 	db \1
 endm
 
 E_: macro
-	dw 261
+	db 4
 	db \1
 endm
 
 F_: macro
-	dw $FA23
+	db 5
 	db \1
 endm
 
 F#: macro
-	dw $FA77
+	db 6
 	db \1
 endm
 
 G_: macro
-	dw $FAC7
+	db 7
 	db \1
 endm
 
 G#: macro
-	dw $FB12
+	db 8
 	db \1
 endm
 
 A_: macro
-	dw $FB58
+	db 9
 	db \1
 endm
 
 A#: macro
-	dw $FB9B
+	db 10
 	db \1
 endm
 
 B_: macro
-	dw $FBDA
+	db 11
 	db \1
 endm
 
 __: macro
-	dw $0000
+	db 12
 	db \1
 endm
 
+octave: macro
+	db 13
+	db \1
+endm
 
 InitSound::
 	ld a, $00
@@ -79,39 +84,62 @@ InitSound::
 	ld [rNR10], a
 	ld a, %11110111
 	ld [rNR12], a ; volume
-	ld a, %10000000
+	ld a, %10000001
 	ld [rNR11], a ; duty
-	ld a, %11000011
+	ld a, %11111111
 	ld [rNR51], a ; 
 	ld a, $40
 	ld [rNR14], a ; counter mode
-	ld [rNR24], a
-	ld [rNR44], a
+	;ld [rNR24], a
+	;ld [rNR44], a
 	ld a, $77
 	ld [rNR50], a
 	ld hl, SongData
-	;ld a, [hli]
-	inc hl
-	xor a
+StartNextNote:
+	ld a, [hli]
+	cp 13
+	jr nz, .notOctave
+	ld a, [hli]
+	ld [wCh0Octave], a
+	ld a, [hli]
+.notOctave
 	ld [wCh0Pitch], a
-	;ld a, [hli]
-	inc hl
-	ld [wCh0Pitch + 1], a
-	ld a, [hl]
+	cp $FF
+	ret z
+	ld c, a
+	ld a, [hli]
 	ld [wCh0Length], a
 	ld a, l
 	ld [wCh0Ptr], a
 	ld a, h
 	ld [wCh0Ptr + 1], a
+	ld a, [wCh0Octave]
+	ld b, a
+	ld a, c
+	cp 12
+	jr z, .rest
+	call CalculateFrequency
+	ld a, [rNR51]
+	or %00010001
+	ld [rNR51], a
+	ld a, e
+	ld [rNR13], a
+	ld a, d
+	ld [rNR14], a
+	ret
+.rest
+	ld a, [rNR51]
+	and %11101110
+	ld [rNR51], a
+	xor a
+	ld [rNR13], a
+	ld [rNR14], a
 	ret
 
 PlaySound:
 	ld a, [wCh0Pitch]
 	cp $FF
 	ret z
-	ld [rNR14], a
-	ld a, [wCh0Pitch + 1]
-	ld [rNR13], a
 	ld a, [wCh0Length]
 	and a
 	jr z, .nextNote
@@ -123,54 +151,76 @@ PlaySound:
 	ld l, a
 	ld a, [wCh0Ptr + 1]
 	ld h, a
-	;ld a, [hli]
-	ld a, [wCh0Pitch]
-	;inc a
-	ld [wCh0Pitch], a
-	;ld a, [hli]
-	ld a, [wCh0Pitch + 1]
-	inc a
-	ld [wCh0Pitch + 1], a
-	ld a, [hl]
-	ld [wCh0Length], a
-	ld a, l
-	ld [wCh0Ptr], a
-	ld a, h
-	ld [wCh0Ptr + 1], a
+	call StartNextNote
 	ret
 
+CalculateFrequency:
+; return the frequency for note a, octave b in de
+	ld h, 0
+	ld l, a
+	add hl, hl
+	ld d, h
+	ld e, l
+	ld hl, Pitches
+	add hl, de
+	ld e, [hl]
+	inc hl
+	ld d, [hl]
+	ld a, b
+.loop
+	cp 7
+	jr z, .done
+	sra d
+	rr e
+	inc a
+	jr .loop
+.done
+	ld a, 8
+	add d
+	ld d, a
+	ret
+
+Pitches:
+	dw $F82C ; C_
+	dw $F89D ; C#
+	dw $F907 ; D_
+	dw $F96B ; D#
+	dw $F9CA ; E_
+	dw $FA23 ; F_
+	dw $FA77 ; F#
+	dw $FAC7 ; G_
+	dw $FB12 ; G#
+	dw $FB58 ; A_
+	dw $FB9B ; A#
+	dw $FBDA ; B_
+
 SongData:
-	C_ 100 ; E
-	C_ 100 ; E
-	C_ 100 ; E
-	C_ 100 ; E
-	C_ 100 ; E
-	C_ 100 ; E
-	C_ 100 ; E
-	C_ 100 ; E
-	C_ 100 ; E
-	C_ 100 ; E
-	C_ 100 ; E
-	C_ 100 ; E
-	C_ 100 ; E
-	C_ 100 ; E
-	C_ 100 ; E
-	C_ 100 ; E
-	C_ 100 ; E
-	;D_ 100 ; X
-	;E_ 100 ; Eb
-	F_ 100
-	F_ 100
-	F_ 100
-	F_ 100
-	F_ 100
-	F_ 100
-	F_ 100
-	F_ 100
-	F_ 100
-	F_ 100
-	;G_ 100 ; X
-	A_ 100
-	B_ 100
-	C_ 100
-	db $FF, $FF, $FF
+	octave 6
+	C_ 50
+	__ 50
+	C# 50
+	__ 50
+	D_ 50
+	__ 50
+	D# 50
+	__ 50
+	E_ 50
+	__ 50
+	F_ 50
+	__ 50
+	F# 50
+	__ 50
+	G_ 50
+	__ 50
+	G# 50
+	__ 50
+	A_ 50
+	__ 50
+	A# 50
+	__ 50
+	B_ 50
+	__ 50
+	octave 5
+	C_ 50
+	__ 50
+	db $FF, $FF
